1e1366d015f4c6af2b8eee60d1905cdbebc174f7
Tomas Neubauer <tomas@quix.io>
2024-12-09 12:36:58 +0000

    Edited 'main.py'

--- telegraf-normalization/main.py
+++ telegraf-normalization/main.py
@@ -12,32 +12,18 @@ output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
 
-def normalize(row: dict):
 
-    output_row = {
-        "timestamp": row["timestamp"],
-    }
-    
-    for key, value in row["fields"].items():
-        parameter_dimension = key.replace("values_", "")
-        name = row["tags"]["name"]
-        output_row[name + "-" + parameter_dimension] = value
+sdf = sdf.set_timestamp(lambda row, *_: row["timestamp"])
 
-    return output_row
-
-
-sdf = sdf.apply(normalize)
-
-sdf = sdf.hopping_window(5000, 250).reduce(lambda state, row: { **state, **row}, lambda row: row).final()
-
-sdf = sdf.apply(lambda row:{
-    "timestamp": row["start"],
-    **row["value"]
+sdf = sdf.apply(lambda row: {
+    **row["fields"],
+    "tags": row["tags"],
+    "name": row["name"]
 })
 
 sdf.print()
 
-sdf.to_topic(output_topic)
+sdf.to_topic(output_topic, key=lambda row: row["name"])
 
 if __name__ == "__main__":
     app.run()
